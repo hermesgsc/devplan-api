2dab4a657e25a5d8e5e3185b720d6657541d5e15
hermesgsc <programadorgabriel22@gmail.com>
2025-09-24 20:25:11 -0300

    auth

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
-import routes from "./routes";
+import routes from "./routes/index.js";
 
 const app = express();
 
